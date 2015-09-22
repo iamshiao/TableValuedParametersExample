78412f7b67a4fc1812b26e8cd1992a66ce9c888f
Circle <iamshiao@gmail.com>
2015-09-22 16:38:23 +0800

    Upload full proj - with db backup

---
+++ MyLab/MyLab/Extention/MyExts.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLab.Extention
+{
+    public static class MyExts
+    {
+        /// <summary>
+        /// Get random string
+        /// </summary>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        public static string GetRandomString(int len)
+        {
+            var str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            var rand = new Random(Guid.NewGuid().GetHashCode());
+            var builder = new StringBuilder();
+            for (var i = 0; i < len; i++)
+            {
+                builder.Append(str[rand.Next(0, str.Length)]);
+            }
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Extension method for lst&lt;obj&gt; to DataTable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="lst"></param>
+        /// <returns></returns>
+        public static DataTable ToDataTable<T>(this IList<T> lst)
+        {
+            PropertyDescriptorCollection props =
+                TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            for (int i = 0; i < props.Count; i++)
+            {
+                PropertyDescriptor prop = props[i];
+                table.Columns.Add(prop.Name, prop.PropertyType);
+            }
+            object[] values = new object[props.Count];
+            foreach (T item in lst)
+            {
+                for (int i = 0; i < values.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item);
+                }
+                table.Rows.Add(values);
+            }
+            return table;
+        }
+    }
+}
---
+++ MyLab/MyLab/Program.cs
@@ -0,0 +1,102 @@
+﻿using MyLab.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MyLab.Extention;
+using System.Diagnostics;
+
+namespace MyLab
+{
+    class Program
+    {
+        static MyLabEntities db = null;
+        static Stopwatch sw = null;
+
+        static void Main(string[] args)
+        {
+            sw = new Stopwatch();
+
+            db = new MyLabEntities();
+            var lst = db.Product.ToList<Product>();
+            //BatchInsertProd(lst);
+
+            UpdateProd(lst);
+            BatchUpdateProd(lst);                        
+            Console.ReadLine();
+        }
+
+        private static string BatchInsertProd(List<Product> lstOfProd)
+        {
+            List<Product> lst = new List<Product>();
+            for (int i = 0; i < 20000; i++)
+            {
+                Product prod = new Product();
+                prod.id = Guid.NewGuid();
+                prod.name = MyExts.GetRandomString(6);
+                prod.descrp = MyExts.GetRandomString(25);
+                prod.lastEditAt = DateTime.Now;
+                lst.Add(prod);
+            }
+            db.Product.AddRange(lst);
+            db.SaveChanges();
+
+            return string.Empty;
+        }
+
+        private static void StirData(List<Product> lstOfProd)
+        {
+            sw.Reset();
+            for (int i = 0; i < 20000; i++)
+            {
+                Product prod = lstOfProd[i];                
+                prod.name = MyExts.GetRandomString(6);
+                prod.descrp = MyExts.GetRandomString(25);
+                prod.lastEditAt = DateTime.Now;
+            }
+        }
+
+        private static void UpdateProd(List<Product> lstOfProd)
+        {            
+            StirData(lstOfProd);
+            sw.Start();
+            db.SaveChanges();
+            sw.Stop();
+            Console.WriteLine("UpdateProd m-sec: " + sw.ElapsedMilliseconds);
+        }
+
+        private static string BatchUpdateProd(List<Product> lstOfProd)
+        {
+            StirData(lstOfProd);
+            sw.Start();
+            string sqlCmd = "sp_BatchEditProd @fromClient, @opMsg out";
+            string outMsg = string.Empty;
+
+            var dt = lstOfProd.ToDataTable();
+            List<SqlParameter> lstOfPars = new List<SqlParameter>() {
+                                               new SqlParameter() {
+                                                   Direction = ParameterDirection.Input,
+                                                   ParameterName = "fromClient",
+                                                   SqlDbType = SqlDbType.Structured,
+                                                   TypeName = "Product",
+                                                   Value = dt
+                                               },
+                                               new SqlParameter() {
+                                                   Direction = ParameterDirection.Output,
+                                                   ParameterName = "opMsg",
+                                                   SqlDbType = SqlDbType.NVarChar,
+                                                   Size = 40,
+                                                   Value = null
+                                               }
+                                           };
+            db.Database.ExecuteSqlCommand(sqlCmd, lstOfPars.ToArray());
+            sw.Stop();
+            Console.WriteLine("BatchUpdateProd m-sec: " + sw.ElapsedMilliseconds);
+            return outMsg;
+        }        
+    }
+}
